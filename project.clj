560cb7d70d0979534f33074a6e10ade01b522a60
Sibi <sibi@psibi.in>
2013-07-21 18:52:20 +0530

    Added dependencies.

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http.async.client "0.5.2"]])
+                 [http.async.client "0.5.2"]
+                 [org.clojure/data.xml "0.0.7"]
+                 [org.clojure/data.zip "0.1.1"]])
