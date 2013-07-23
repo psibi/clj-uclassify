61d9b59288d2accc724def5bbc9276a0cd848e4c
Sibi <sibi@psibi.in>
2013-07-24 04:41:45 +0530

    Test functions added for create-classifier.

--- test/clj_uclassify/core_test.clj
+++ test/clj_uclassify/core_test.clj
@@ -2,10 +2,14 @@
   (:require [clojure.test :refer :all]
             [clj-uclassify.core :refer :all]))
 
-(deftest a-test
+(deftest canary-test
   (testing "Canary test"
     (is (= 0 0))))
 
-(deftest check-keys-test
-  (testing "check-keys with two keys"
-    (is (= (check-keys {:read-key "hi" :write-key nil})))))
+(def api-keys {:read-key "aD02ApbU29kNOG2xezDGXPEIck" :write-key "fsqAft7Hs29BgAc1AWeCIWdGnY"})
+(def classifer-name (str (System/currentTimeMillis)))
+
+(deftest create-classifier-test
+  (testing "Creating Classifier test"
+    (is (= (create-classifier api-keys classifer-name) true) "Create New Classifer")
+    (is (thrown? Throwable (create-classifier api-keys classifer-name)) "Creating Existing classifier")))
