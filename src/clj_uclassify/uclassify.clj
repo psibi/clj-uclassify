471e76aaba1789740f2902c2c8ceba41d6dd994d
Sibi <sibi@psibi.in>
2013-07-29 01:20:27 +0530

    Functions added for training the classifier and getting information about the classifier.
    But, get-information functions still needs more work upon it.

--- src/clj_uclassify/uclassify.clj
+++ src/clj_uclassify/uclassify.clj
@@ -2,7 +2,8 @@
   (:require [clj-uclassify.core :refer :all]
             [clojure.data.xml :as xml]
             [clojure.zip :as zip])
-  (:use [clojure.string :only (join)]))
+  (:use [clojure.string :only (join)]
+        [clojure.data.codec.base64 :only (encode)]))
 
 (defn create-classifier [keys classifier]
   (if (check-keys keys)
@@ -65,8 +66,44 @@
     (throw (Throwable. "API Key not found"))))
 
 
-(defn append-elements
-  "Pass an xml-node and an sequence of xml-node, it will return the appended xml-node"
-  [xml-node xml-nodes]
-  (zip/root
-   (reduce #(zip/append-child %1 %2) (zip/xml-zip xml-node) xml-nodes)))
+;(get-information api-keys "some_clas")
+
+(defn get-information
+  "Returns information about the classifier
+TODO: Return response properly instead of true"
+  [keys classifier]
+  (let [info-tag (make-xml-node :getInformation
+                                {:id "GetInformation"
+                                 :classifierName classifier})
+        read-calls (make-xml-node :readCalls
+                                     {:readApiKey (keys :read-key)}
+                                     info-tag)
+        final-xml (zip/root (zip/insert-child
+                             (zip/xml-zip uclassify)
+                             (zip/xml-zip read-calls)))]
+    (post-request
+     (xml/emit-str final-xml))))
+
+(defn train
+  "Trains the classifier on text for a specified class"
+  [keys texts class-name classifier]
+  (let [textbase64-tag (map #(make-xml-node :textBase64
+                                       {:id (str "Text" (str (index-of % texts)))}
+                                       (String. (encode (.getBytes %)) )) texts)
+        train-tag (map #(make-xml-node :train
+                                       {:id (str "Train" %) :className class-name
+                                        :textId (str "Text" %) })
+                       (range (count texts)))
+        texts-tag (make-xml-node :texts {} textbase64-tag)
+        write-calls (make-xml-node :writeCalls
+                                   {:writeApiKey (keys :write-key) :classifierName classifier}
+                                   train-tag)
+        ]
+    (post-request
+     (xml/emit-str
+      (xml-append-elements uclassify (list texts-tag write-calls))))))
+
+;(def api-keys {:read-key "aD02ApbU29kNOG2xezDGXPEIck" :write-key "fsqAft7Hs29BgAc1AWeCIWdGnY"})
+;(train api-keys '("hi" "bye") "male" "gender_classifier")
+
+
