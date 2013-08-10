62ae5dfaf90ee70f87b73a5365c87a36bb0ff706
Sibi <sibi@psibi.in>
2013-08-11 03:40:50 +0530

    Description and url updated.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-uclassify "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "uClassify API Library"
+  :url "https://github.com/psibi/clj-uclassify"
   :license {:name "GNU General Public License Version 3"
             :url "http://www.gnu.org/licenses/gpl.txt"}
   :dependencies [[org.clojure/clojure "1.5.1"]
