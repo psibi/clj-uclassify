d8894803dafb9ae34d545ee4f7865b4a6f904fc2
Sibi <sibi@psibi.in>
2013-08-11 03:37:02 +0530

    Migrated License to GNU GPL v3.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-uclassify "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License Version 3"
+            :url "http://www.gnu.org/licenses/gpl.txt"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http.async.client "0.5.2"]
                  [org.clojure/data.xml "0.0.7"]
