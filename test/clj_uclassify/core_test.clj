6e1026fc52e7e195431b5ee0e30db86c05718534
Sibi <sibi@psibi.in>
2013-08-30 03:07:05 +0530

    Names changed to make them more sensible.

--- test/clj_uclassify/core_test.clj
+++ test/clj_uclassify/core_test.clj
@@ -9,14 +9,16 @@
 (def ^:private api-keys {:read-key "aD02ApbU29kNOG2xezDGXPEIck" :write-key "fsqAft7Hs29BgAc1AWeCIWdGnY"})
 (def ^:private classifer-name (str (System/currentTimeMillis)))
 
-(deftest create-classifier-test
+(deftest uclassify-test
   (testing "API test with two classes"
     (is (= (create-classifier api-keys classifer-name) true) "Create New Classifer")
     (is (= (add-class api-keys classifer-name '("class1" "class2")) true) "Creates some new class labels in Classifier")
     (is (= (get-information api-keys classifer-name) '(("class1" "class2") ("0" "0") ("0" "0"))) "get information about classifier")
-    (is (= (classify api-keys '("hi" "bye") classifer-name) '(("0" "0") (["class1" "0.5"] ["class2" "0.5"]) (["class1" "0.5"] ["class2" "0.5"])))
+    (is (= (classify api-keys '("hi" "bye") classifer-name)
+           '(("0" "0") (["class1" "0.5"] ["class2" "0.5"]) (["class1" "0.5"] ["class2" "0.5"])))
         "Classify the passed texts")
-    (is (= (classify-keywords api-keys '("hi" "bye") classifer-name) '(("0" "0") (["class1" "0.5" ""] ["class2" "0.5" ""]) (["class1" "0.5" ""] ["class2" "0.5" ""]))))
+    (is (= (classify-keywords api-keys '("hi" "bye") classifer-name)
+           '(("0" "0") (["class1" "0.5" ""] ["class2" "0.5" ""]) (["class1" "0.5" ""] ["class2" "0.5" ""]))))
     (is (= (classify api-keys '("that cosmetic is so nice" "I like to code.") "GenderAnalyzer_v5" "uClassify")
            '(("1" "1") (["female" "0.844033"] ["male" "0.155967"]) (["female" "0.146065"] ["male" "0.853935"])))
         "Classify using predefined classifier which is already present")
@@ -30,14 +32,16 @@
 
 (def ^:private another-classifier (str classifer-name \a))
 
-(deftest another-classifier-test
+(deftest another-uclassify-test
   (testing "Another API test with three classes"
     (is (= (create-classifier api-keys another-classifier) true) "Create New Classifer")
     (is (= (add-class api-keys another-classifier '("class1" "class2" "class3")) true) "Creates some new class labels in Classifier")
-    (is (= (get-information api-keys another-classifier) '(("class1" "class2" "class3") ("0" "0" "0") ("0" "0" "0"))) "get information about classifier")
-    (is (= (classify api-keys '("hi" "bye") another-classifier) '(("0" "0") (["class1" "0.333333"] ["class2" "0.333333"] ["class3" "0.333333"]) (["class1" "0.333333"] ["class2" "0.333333"] ["class3" "0.333333"])))
-        "Classify the passed texts")
-    (is (= (classify-keywords api-keys '("hi" "bye") another-classifier) '(("0" "0") (["class1" "0.333333" ""] ["class2" "0.333333" ""] ["class3" "0.333333" ""]) (["class1" "0.333333" ""] ["class2" "0.333333" ""] ["class3" "0.333333" ""]))))
+    (is (= (get-information api-keys another-classifier)
+           '(("class1" "class2" "class3") ("0" "0" "0") ("0" "0" "0"))) "get information about classifier")
+    (is (= (classify api-keys '("hi" "bye") another-classifier)
+           '(("0" "0") (["class1" "0.333333"] ["class2" "0.333333"] ["class3" "0.333333"]) (["class1" "0.333333"] ["class2" "0.333333"] ["class3" "0.333333"]))) "Classify the passed texts")
+    (is (= (classify-keywords api-keys '("hi" "bye") another-classifier)
+           '(("0" "0") (["class1" "0.333333" ""] ["class2" "0.333333" ""] ["class3" "0.333333" ""]) (["class1" "0.333333" ""] ["class2" "0.333333" ""] ["class3" "0.333333" ""]))))
     (is (= (train api-keys '("I like cosmetic" "That is so hot") "class1" another-classifier) true) "Train a classifier")
     (is (= (untrain api-keys '("I like cosmetic" "That is so hot") "class1" another-classifier) true) "UnTrain a classifier")
     (is (= (remove-class api-keys another-classifier '("class1" "class2")) true) "Removes the created class labels")
